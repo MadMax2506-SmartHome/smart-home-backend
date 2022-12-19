0114b91ac32c5fe17b42877e5002077cd2ffb733
Max Janorschke <programming@max-janorschke.de>
2022-12-19 21:22:13 +0100

    implement mqtt
    * add env file
    * move python files to app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from mqtt import Mqtt
-from topic import Topic
-from utils import string_to_json, update_received_devices
+from app.mqtt import Mqtt
+from app.topic import Topic
+from app.utils import string_to_json, update_received_devices
 
-client = Mqtt(broker="192.168.178.150", port=1883, client_id="smarthome-client")
+load_dotenv()
+
+client = Mqtt(
+    broker=os.getenv('BROKER_ADDRESS'),
+    port=int(os.getenv('BROKER_PORT')),
+    client_id=os.getenv('BROKER_CLIENT_ID')
+)
 devices_topic = Topic(
     client,
     topic_prefix="devices",
@@ -30,5 +39,5 @@ async def load_devices():
 
 @app.get("/api/v1/devices")
 async def devices():
-    return devices_topic.get_subscribed_messages()\
+    return devices_topic.get_subscribed_messages()
 
